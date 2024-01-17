1b192d9990574b150275e8100157ae261b8043af
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-17 21:42:33 +0100

    Add getPostBySlug method to PostRepository

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -54,4 +54,41 @@ class PostRepository
     {
         return $this->queryActive()->with('categories')->latest('updated_at')->take(5)->get();
     }
+
+    public function getPostBySlug($slug)
+{
+    // Post for slug with user, tags and categories
+    $post = Post::with(
+                'user:id,name,email',
+                'tags:id,tag,slug',
+                'categories:title,slug'
+            )
+            ->withCount('validComments')
+            ->whereSlug($slug)
+            ->firstOrFail();
+
+    // Previous post
+    $post->previous = $this->getPreviousPost($post->id);
+
+    // Next post
+    $post->next = $this->getNextPost($post->id);
+
+    return $post;
+}
+
+protected function getPreviousPost($id)
+{
+    return Post::select('title', 'slug')
+                ->whereActive(true)
+                ->latest('id')
+                ->firstWhere('id', '<', $id);
+}
+
+protected function getNextPost($id)
+{
+    return Post::select('title', 'slug')
+                ->whereActive(true)
+                ->oldest('id')
+                ->firstWhere('id', '>', $id);
+}
 }
