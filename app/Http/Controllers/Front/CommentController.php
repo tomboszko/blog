a8ef04fd0c2b759bc582e21b4db4fdee915f2991
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 18:01:16 +0100

    Refactor CommentController's destroy method to delete a specific comment
    This commit refactors the `destroy` method in the `CommentController` class to accept a `Comment` object as a parameter. It then authorizes the deletion of the comment, deletes it, and returns a JSON response.

--- app/Http/Controllers/Front/CommentController.php
+++ app/Http/Controllers/Front/CommentController.php
@@ -28,9 +28,11 @@ class CommentController extends Controller
     return response()->json($commenter->valid ? 'ok' : 'invalid');
 }
 
-    public function destroy()
+    public function destroy(Comment $comment)
     {
-
+        $this->authorize('delete', $comment);
+        $comment->delete();
+        return response()->json();
     }
 
     public function comments(Post $post)
