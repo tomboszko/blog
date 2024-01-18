d7fc2e6b3fa9a152800f007c15c5fad349b19e11
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 11:27:41 +0100

    Refactor PostRepository.php to improve code readability and add new methods for retrieving posts by slug, category, user, tag, and search.

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -55,40 +55,120 @@ class PostRepository
         return $this->queryActive()->with('categories')->latest('updated_at')->take(5)->get();
     }
 
+    /**
+     * Get post by slug.
+     *
+     * @param  string  $slug
+     * @return array
+     */
     public function getPostBySlug($slug)
-{
-    // Post for slug with user, tags and categories
-    $post = Post::with(
-                'user:id,name,email',
-                'tags:id,tag,slug',
-                'categories:title,slug'
-            )
-            ->withCount('validComments')
-            ->whereSlug($slug)
-            ->firstOrFail();
+    {
+        // Post for slug with user, tags and categories
+        $post = Post::with(
+                    'user:id,name,email',
+                    'tags:id,tag,slug',
+                    'categories:title,slug'
+                )
+                ->withCount('validComments')
+                ->whereSlug($slug)
+                ->firstOrFail();
+    
+        // Previous post
+        $post->previous = $this->getPreviousPost($post->id);
+    
+        // Next post
+        $post->next = $this->getNextPost($post->id);
+    
+        return $post;
+    }
+    
+    /**
+     * Get previous post
+     *
+     * @param  integer  $id
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function getPreviousPost($id)
+    {
+        return Post::select('title', 'slug')
+                    ->whereActive(true)
+                    ->latest('id')
+                    ->firstWhere('id', '<', $id);
+    }
+    
+    /**
+     * Get next post
+     *
+     * @param  integer  $id
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function getNextPost($id)
+    {
+        return Post::select('title', 'slug')
+                    ->whereActive(true)
+                    ->oldest('id')
+                    ->firstWhere('id', '>', $id);
+    }
 
-    // Previous post
-    $post->previous = $this->getPreviousPost($post->id);
+    /**
+     * Get active posts for specified category.
+     *
+     * @param  int  $nbrPages
+     * @param  string  $category_slug
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getActiveOrderByDateForCategory($nbrPages, $category_slug)
+    {
+        return $this->queryActiveOrderByDate()
+                    ->whereHas('categories', function ($q) use ($category_slug) {
+                        $q->where('categories.slug', $category_slug);
+                    })->paginate($nbrPages);
+    }
 
-    // Next post
-    $post->next = $this->getNextPost($post->id);
+    /**
+     * Get active posts for specified user.
+     *
+     * @param  int  $nbrPages
+     * @param  integer  $user_id
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getActiveOrderByDateForUser($nbrPages, $user_id)
+    {
+        return $this->queryActiveOrderByDate()
+                    ->whereHas('user', function ($q) use ($user_id) {
+                        $q->where('users.id', $user_id);
+                    })->paginate($nbrPages);
+    }
 
-    return $post;
-}
+    /**
+     * Get active posts for specified tag.
+     *
+     * @param  int  $nbrPages
+     * @param  int  $tag_id
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getActiveOrderByDateForTag($nbrPages, $tag_slug)
+    {
+        return $this->queryActiveOrderByDate()
+                    ->whereHas('tags', function ($q) use ($tag_slug) {
+                        $q->where('tags.slug', $tag_slug);
+                    })->paginate($nbrPages);
+    }
 
-protected function getPreviousPost($id)
-{
-    return Post::select('title', 'slug')
-                ->whereActive(true)
-                ->latest('id')
-                ->firstWhere('id', '<', $id);
-}
-
-protected function getNextPost($id)
-{
-    return Post::select('title', 'slug')
-                ->whereActive(true)
-                ->oldest('id')
-                ->firstWhere('id', '>', $id);
-}
+    /**
+     * Get posts with search.
+     *
+     * @param  int  $n
+     * @param  string  $search
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function search($n, $search)
+    {
+        return $this->queryActiveOrderByDate()
+                    ->where(function ($q) use ($search) {
+                        $q->where('excerpt', 'like', "%$search%")
+                          ->orWhere('body', 'like', "%$search%")
+                          ->orWhere('title', 'like', "%$search%");
+                    })->paginate($n);
+    }
 }
