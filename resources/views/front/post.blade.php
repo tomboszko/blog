fdc7624f5c493639eb947b57136cff615c54cb87
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 16:39:19 +0100

    Refactor post.blade.php and add show comments functionality

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -34,31 +34,31 @@
                           <img class="avatar" src="{{ Gravatar::get($post->user->email) }}" alt="">
                       </div>
                       <div class="byline">
-                        <span class="bytext">@lang('Posted By')</span>
-                        <a href="{{ route('author', $post->user->id) }}">{{ $post->user->name }}</a>
-                    </div>
+                          <span class="bytext">@lang('Posted By')</span>
+                          <a href="{{ route('author', $post->user->id) }}">{{ $post->user->name }}</a>
+                      </div>
                   </div>
 
                   <div class="meta-bottom">
                       
                       <div class="entry-cat-links meta-blk">
-                        <div class="cat-links">
-                            <span>@lang('In')</span> 
-                            @foreach ($post->categories as $category)
-                                <a href="{{ route('category', $category->slug) }}">{{ $category->title }}</a>
-                            @endforeach
-                        </div>
+                          <div class="cat-links">
+                              <span>@lang('In')</span> 
+                              @foreach ($post->categories as $category)
+                                  <a href="{{ route('category', $category->slug) }}">{{ $category->title }}</a>
+                              @endforeach
+                          </div>
 
                           <span>@lang('On')</span>
                           {{ formatDate($post->created_at) }}
                       </div>
 
                       <div class="entry-tags meta-blk">
-                        <span class="tagtext">@lang('Tags')</span>
-                        @foreach($post->tags as $tag)
-                            <a href="{{ route('tag', $tag->slug) }}">{{ $tag->tag }}</a>
-                        @endforeach
-                    </div>
+                          <span class="tagtext">@lang('Tags')</span>
+                          @foreach($post->tags as $tag)
+                              <a href="{{ route('tag', $tag->slug) }}">{{ $tag->tag }}</a>
+                          @endforeach
+                      </div>
 
                   </div>
 
@@ -91,24 +91,82 @@
 
 <!-- comments
 ================================================== -->
-    <div class="comments-wrap">
+<div class="comments-wrap">
 
-        <div id="comments" class="row">
-            <div id="commentsList" class="column large-12">      
-    
-                @if($post->valid_comments_count > 0)
-                    <div id="forShow">
-                        <p id="showbutton" class="text-center">
-                            <a id="showcomments" href="{{ route('posts.comments', $post->id) }}" class="btn h-full-width">@lang('Show comments')</a>
-                        </p>
-                        <p id="showicon" class="h-text-center" hidden>
-                            <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
-                        </p>
-                    </div>
-                @endif
-    
-            </div>
-        </div>
-    </div>
+  <div id="comments" class="row">
+      <div id="commentsList" class="column large-12">      
 
+          @if($post->valid_comments_count > 0)
+              <div id="forShow">
+                  <p id="showbutton" class="text-center">
+                      <a id="showcomments" href="{{ route('posts.comments', $post->id) }}" class="btn h-full-width">@lang('Show comments')</a>
+                  </p>
+                  <p id="showicon" class="h-text-center" hidden>
+                      <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
+                  </p>
+              </div>
+          @endif
+
+      </div>
+  </div>
+</div>
+
+@endsection
+
+@section('scripts')
+    <script>
+      (() => {
+
+          // Variables
+          const headers = {
+              'X-CSRF-TOKEN': '{{ csrf_token() }}', 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'X-Requested-With': 'XMLHttpRequest'
+          }
+
+          // Prepare show comments
+          const prepareShowComments = e => {
+              e.preventDefault();
+
+              document.getElementById('showbutton').toggleAttribute('hidden');
+              document.getElementById('showicon').toggleAttribute('hidden');
+              showComments(); 
+          }
+
+          // Show comments
+          const showComments = async () => {
+
+              // Send request
+              const response = await fetch('{{ route('posts.comments', $post->id) }}', { 
+                  method: 'GET',
+                  headers: headers
+              });
+
+              // Wait for response
+              const data = await response.json();
+
+              document.getElementById('commentsList').innerHTML = data.html;
+          }
+
+          // Listener wrapper
+          const wrapper = (selector, type, callback, condition = 'true', capture = false) => {
+              const element = document.querySelector(selector);
+              if(element) {
+                  document.querySelector(selector).addEventListener(type, e => { 
+                      if(eval(condition)) {
+                          callback(e);
+                      }
+                  }, capture);
+              }
+          };
+
+          // Set listeners
+          window.addEventListener('DOMContentLoaded', () => {
+              wrapper('#showcomments', 'click', prepareShowComments);              
+          })
+
+      })()
+
+    </script> 
 @endsection
