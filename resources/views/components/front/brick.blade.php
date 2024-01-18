749b8c50ef8afac1984cb9524bc055143b1ec6db
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 13:35:52 +0100

    Update links in brick.blade.php

--- resources/views/components/front/brick.blade.php
+++ resources/views/components/front/brick.blade.php
@@ -3,18 +3,18 @@
 <article class="brick entry" data-aos="fade-up">
 
   <div class="entry__thumb">
-      <a href="#" class="thumb-link">
+      <a href="{{ route('posts.display', $post->slug) }}" class="thumb-link">
           <img src="{{ getImage($post, true) }}" alt="" style="width:100%">
       </a>
   </div>
 
   <div class="entry__text">
       <div class="entry__header">
-          <h1 class="entry__title"><a href="#">{{ $post->title }}</a></h1>          
+          <h1 class="entry__title"><a href="{{ route('posts.display', $post->slug) }}">{{ $post->title }}</a></h1>          
           <div class="entry__meta">
               <span class="byline"">@lang('By:')
                   <span class='author'>
-                      <a href="#">{{ $post->user->name }}</a>
+                      <a href="{{ route('author', $post->user->id) }}">{{ $post->user->name }}</a>
                   </span>
               </span>
           </div>
@@ -22,7 +22,7 @@
       <div class="entry__excerpt">
           <p>{{ $post->excerpt }}</p>
       </div>
-      <a class="entry__more-link" href="#">@lang('Read More')</a>
+      <a class="entry__more-link" href="{{ route('posts.display', $post->slug) }}">@lang('Read More')</a>
   </div>
 
 </article>
