e24af81905c61f23b35a27c59ff755754fec059e
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 20:12:50 +0100

    Add input password component

---
+++ resources/views/components/auth/input-password.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <label for="password">@lang('Password')</label> 
+    <input 
+        id="password" 
+        class="h-full-width" 
+        type="password" 
+        name="password" 
+        placeholder="@lang('Your password')" 
+        required>
+</div>
