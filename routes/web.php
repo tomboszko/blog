78373c9502d3a497bdb6fc464e8d39b749a41ed2
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-18 13:17:39 +0100

    Add SEO configuration and Post routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::name('category')->get('category/{category:slug}', [FrontPostController::c
 Route::name('author')->get('author/{user}', [FrontPostController::class, 'user']);
 Route::name('tag')->get('tag/{tag:slug}', [FrontPostController::class, 'tag']);
 
+// Post
 Route::prefix('posts')->group(function () {
     Route::name('posts.display')->get('{slug}', [FrontPostController::class, 'show']);
     Route::name('posts.search')->get('', [FrontPostController::class, 'search']);
