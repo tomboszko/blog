d17f4351bcdbf61b977890fb4b15db73e9929903
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-20 19:57:37 +0100

    added index back dashboard

---
+++ resources/views/back/shared/index.blade.php
@@ -0,0 +1,60 @@
+@extends('back.layout')
+
+@section('css')
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+  <style>
+    a > * { pointer-events: none; }
+  </style>
+@endsection
+
+@section('main') 
+  {{ $dataTable->table(['class' => 'table table-bordered table-hover table-sm'], true) }}
+@endsection
+
+@section('js') 
+  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script> 
+  <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+  @if(config('app.locale') == 'fr')
+    <script>
+      (($, DataTable) => {
+        $.extend(true, DataTable.defaults, {
+          language: {
+            "sEmptyTable":     "Aucune donnée disponible dans le tableau",
+            "sInfo":           "Affichage des éléments _START_ à _END_ sur _TOTAL_ éléments",
+            "sInfoEmpty":      "Affichage de l'élément 0 à 0 sur 0 élément",
+            "sInfoFiltered":   "(filtré à partir de _MAX_ éléments au total)",
+            "sInfoPostFix":    "",
+            "sInfoThousands":  ",",
+            "sLengthMenu":     "Afficher _MENU_ éléments",
+            "sLoadingRecords": "Chargement...",
+            "sProcessing":     "Traitement...",
+            "sSearch":         "Rechercher :",
+            "sZeroRecords":    "Aucun élément correspondant trouvé",
+            "oPaginate": {
+              "sFirst":    "Premier",
+              "sLast":     "Dernier",
+              "sNext":     "Suivant",
+              "sPrevious": "Précédent"
+            },
+            "oAria": {
+              "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+              "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
+            },
+            "select": {
+              "rows": {
+                "_": "%d lignes sélectionnées",
+                "0": "Aucune ligne sélectionnée",
+                "1": "1 ligne sélectionnée"
+              }  
+            }
+          }
+        });
+      })(jQuery, jQuery.fn.dataTable);
+    </script>
+  @endif
+
+  {{ $dataTable->scripts() }}
+
+@endsection
