ced74fc24449654b2edf8bb70bec800155fe894c
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 14:40:00 +0100

    Add validation rules for title, body, and slug in PageRequest.php

--- app/Http/Requests/Back/PageRequest.php
+++ app/Http/Requests/Back/PageRequest.php
@@ -3,28 +3,22 @@
 namespace App\Http\Requests\Back;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\Slug;
 
 class PageRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
-        return [
-            //
+        $id = basename($this->url());
+
+        return $rules = [
+            'title' => 'required|max:255',
+            'body' => 'required|max:65000',
+            'slug' => ['required', 'max:255', new Slug, 'unique:posts,slug,' . $id],
         ];
     }
 }
