ec3d9738c3e852c592a731d5997f2bf8e04619fa
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 10:35:53 +0100

    Add menu items for Posts and update titles configuration

--- config/menu.php
+++ config/menu.php
@@ -7,6 +7,30 @@ return [
         'route'  => 'admin',
         'icon'   => 'tachometer-alt',
     ],
-
-    //ajout menu itmes ici
+    'Posts' => [
+        'icon' => 'file-alt',
+        'role'   => 'redac',
+        'children' => [
+            [
+                'name'  => 'All posts',
+                'role'  => 'redac',
+                'route' => 'posts.index',
+            ],
+            [
+                'name'  => 'New posts',
+                'role'  => 'admin',
+                'route' => 'posts.indexnew',
+            ],
+            [
+                'name'  => 'Add',
+                'role'  => 'redac',
+                'route' => 'posts.create',
+            ],
+            [
+                'name'  => 'fake',
+                'role'  => 'redac',
+                'route' => 'posts.edit',
+            ],
+        ],
+    ],
 ];
--- config/titles.php
+++ config/titles.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+
     'admin' => 'Dashboard',
+    'posts' => [
+        'index'    => 'Posts',
+        'create'   => 'Post Creation',
+        'edit'     => 'Post Edit',
+        'indexnew' => 'New Posts',
+    ],
 ];
