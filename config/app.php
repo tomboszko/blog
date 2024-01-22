b8a9c51ea523ce735b49c9667c1c6cfa1e4ebaec
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 19:16:00 +0100

    Add GravatarServiceProvider to app.php

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Creativeorange\Gravatar\GravatarServiceProvider::class,
 
         /*
          * Package Service Providers...
