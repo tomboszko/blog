8b5cb34068bfb7d7b308a5fb2020a8b4d4672db3
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 11:35:27 +0100

    Add delete functionality to shared index page

--- resources/views/back/shared/index.blade.php
+++ resources/views/back/shared/index.blade.php
@@ -57,4 +57,67 @@
 
   {{ $dataTable->scripts() }}
 
+  <script>
+    (() => {
+
+        // Variables
+        const headers = {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}', 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+   
+        // Delete 
+        const deleteElement = async e => {              
+            e.preventDefault();
+            Swal.fire({
+              title: e.target.dataset.name,
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#DD6B55',
+              confirmButtonText: '@lang('Yes')',
+              cancelButtonText: '@lang('No')',
+              preConfirm: () => {
+                  return fetch(e.target.getAttribute('href'), { 
+                      method: 'DELETE',
+                      headers: headers
+                  })
+                  .then(response => {
+                      if (response.ok) {
+                          e.target.parentNode.parentNode.remove();
+                      } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: '@lang('Whoops!')',
+                            text: '@lang('Something went wrong!')'
+                        });  
+                      }
+                  });
+              }
+            });
+        }
+
+        // Listener wrapper
+        const wrapper = (selector, type, callback, condition = 'true', capture = false) => {
+            const element = document.querySelector(selector);
+            if(element) {
+                document.querySelector(selector).addEventListener(type, e => { 
+                    if(eval(condition)) {
+                        callback(e);
+                    }
+                }, capture);
+            }
+        };
+
+        // Set listeners
+        window.addEventListener('DOMContentLoaded', () => {
+            wrapper('table', 'click', deleteElement, "e.target.matches('.btn-danger')");
+        });
+
+    })()
+
+  </script> 
+
+@endsection
+
 @endsection
