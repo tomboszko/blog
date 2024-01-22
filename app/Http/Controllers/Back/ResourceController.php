15a6422fb3bd0c65e24d19f407e473dd92e265a3
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 11:44:41 +0100

    Update user authentication logic to use bcrypt for password hashing

---
+++ app/Http/Controllers/Back/ResourceController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers\Back;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
+class ResourceController extends Controller
+{
+    protected $dataTable;
+    protected $view;
+    protected $formRequest;
+    protected $singular;
+}
