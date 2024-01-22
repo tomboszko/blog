27776b6be37a5e7d10ba72879fcbf8c8db720777
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-01-22 12:02:49 +0100

    Add titles for admin routes and categories

--- config/titles.php
+++ config/titles.php
@@ -2,11 +2,24 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Titles for routes names
+    |--------------------------------------------------------------------------
+    |
+    | Set Titles for each admin routes names
+    */
+
     'admin' => 'Dashboard',
     'posts' => [
         'index'    => 'Posts',
         'create'   => 'Post Creation',
         'edit'     => 'Post Edit',
         'indexnew' => 'New Posts',
+    ],
+    'categories' => [
+        'index'   => 'Categories',
+        'create'  => 'Category Creation',
+        'edit'    => 'Category Edit',
     ],
 ];
